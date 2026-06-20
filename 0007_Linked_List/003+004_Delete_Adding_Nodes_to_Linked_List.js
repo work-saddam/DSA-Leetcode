41e64e51bf43769bf56320149b711aebafa825db
Saddam Hussein <work.saddam786@gmail.com>
2026-06-20 23:08:13 +0530

    add implementation of MyLinkedList with methods for adding, getting, and deleting nodes

--- 0007_Linked_List/003+004_Delete_Adding_Nodes_to_Linked_List.js
+++ 0007_Linked_List/003+004_Delete_Adding_Nodes_to_Linked_List.js
@@ -14,3 +14,85 @@ class MyLinkedList {
     this.size = 0;
   }
 }
+
+MyLinkedList.prototype.addAtHead = function (val) {
+  let newNode = new Node(val);
+  newNode.next = this.head;
+  this.head = newNode;
+  this.size++;
+};
+
+MyLinkedList.prototype.addAtTail = function (val) {
+  let newNode = new Node(val);
+  if (this.head === null) {
+    this.head = newNode;
+  } else {
+    let curr = this.head;
+    while (curr.next !== null) {
+      curr = curr.next;
+    }
+    curr.next = newNode;
+  }
+  this.size++;
+};
+
+MyLinkedList.prototype.addAtIndex = function (index, val) {
+  if (index < 0 || index > this.size) {
+    return;
+  }
+  let newNode = new Node(val);
+  if (index === 0) {
+    // return this.addAtHead(val)
+
+    newNode.next = this.head;
+    this.head = newNode;
+  } else {
+    let curr = this.head;
+    for (let i = 0; i < index - 1; i++) {
+      curr = curr.next;
+    }
+    newNode.next = curr.next;
+    curr.next = newNode;
+  }
+  this.size++;
+};
+
+MyLinkedList.prototype.get = function (index) {
+  if (index < 0 || index >= this.size) return -1;
+
+  let curr = this.head;
+  for (let i = 0; i < index; i++) {
+    curr = curr.next;
+  }
+  return curr.val;
+};
+
+MyLinkedList.prototype.deleteAtIndex = function (index) {
+  if (index < 0 || index >= this.size) return;
+
+  if (index === 0) {
+    this.head = this.head.next;
+  } else {
+    let curr = this.head;
+    for (let i = 0; i < index - 1; i++) {
+      curr = curr.next;
+    }
+    curr.next = curr.next.next;
+  }
+  this.size--;
+};
+
+const list = new MyLinkedList();
+console.log(list);
+
+list.addAtHead(2); // [2]
+list.addAtTail(4); // [2 → 4]
+list.addAtIndex(1, 3); // [2 → 3 → 4]
+console.log(list);
+
+console.log(list.get(1)); // 3
+
+list.deleteAtIndex(1); // remove 3
+console.log(list);
+
+console.log(list.get(1)); // 4
