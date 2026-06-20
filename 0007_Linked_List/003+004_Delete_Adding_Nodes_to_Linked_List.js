f97f571f4e77c0949c058635a5fbfd61cb5d1728
Saddam Hussein <work.saddam786@gmail.com>
2026-06-20 23:07:49 +0530

    add implementation of MyLinkedList with Node structure

---
+++ 0007_Linked_List/003+004_Delete_Adding_Nodes_to_Linked_List.js
@@ -0,0 +1,16 @@
+// 707. Design Linked List
+// Leetcode: https://leetcode.com/problems/design-linked-list/description/
+
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.next = null;
+  }
+}
+
+class MyLinkedList {
+  constructor() {
+    this.head = null;
+    this.size = 0;
+  }
+}
