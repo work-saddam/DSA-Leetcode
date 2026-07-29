dc58535ad3de31f534d2f1aa37a6518689e6d513
Saddam Hussein <work.saddam786@gmail.com>
2026-07-29 22:19:27 +0530

    add approach2 for balancedStringSplit

--- 0008_Strings/006_Split_a_String_in_Balanced_String.js
+++ 0008_Strings/006_Split_a_String_in_Balanced_String.js
@@ -2,22 +2,43 @@
 // Leetcode: https://leetcode.com/problems/split-a-string-in-balanced-strings/
 
 //  Approach 1
+// var balancedStringSplit = function (s) {
+//     let maxBalancedString = 0
+//     let r = 0
+//     let l = 0
+
+//     for (let i = 0; i < s.length; i++) {
+//         if (s[i] === 'R') {
+//             r++
+//         } else {
+//             l++
+//         }
+
+//         if (l === r) {
+//             maxBalancedString++
+//             r = 0
+//             l = 0
+//         }
+//     }
+//     return maxBalancedString
+// };
+// Time Complexity = O(n)
+// Space Complexity = O(1)
+
+//  Approach 2: little optimised
 var balancedStringSplit = function (s) {
   let maxBalancedString = 0;
-  let r = 0;
-  let l = 0;
+  let temp = 0;
 
   for (let i = 0; i < s.length; i++) {
     if (s[i] === "R") {
-      r++;
+      temp++;
     } else {
-      l++;
+      temp--;
     }
 
-    if (l === r) {
+    if (temp === 0) {
       maxBalancedString++;
-      r = 0;
-      l = 0;
     }
   }
   return maxBalancedString;
